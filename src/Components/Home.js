f3318831610f0cb9f37ff404e716f6abd82d20fd
Tanisha <tanishagupta0820@gmail.com>
2023-03-05 19:30:33 +0530

    footer

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,6 +4,8 @@ import Facilities from './Facilities.js';
 import './Home.css';
 import Image from '../assets/logos/marky.jpg';
 import brochure1 from '../assets/brochures/SSSbrochure1.jpg'
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import {Link} from 'react-router-dom'
 
 function Home() {
   return (
@@ -56,10 +58,41 @@ function Home() {
       {/* M   A   P */}
 
       <div className="googlemap">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.074196212632!2d76.81172885013075!3d30.518941002867138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fc0bd4ec5dc99%3A0xf572f957b02182c0!2sShivam%20Smart%20School%2C%20Dappar!5e0!3m2!1sen!2sin!4v1678012964288!5m2!1sen!2sin" width="1050" height="500" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.074196212632!2d76.81172885013075!3d30.518941002867138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fc0bd4ec5dc99%3A0xf572f957b02182c0!2sShivam%20Smart%20School%2C%20Dappar!5e0!3m2!1sen!2sin!4v1678012964288!5m2!1sen!2sin" width="1050" height="500" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
       {/* F   O   O   T   E   R  */}
+
+      <div className='school-footer'>
+        <div className='school-contact-no info-margin-top'>
+          <ul className='ul-properties'>
+            <li>999999999</li>
+            <li>999999999</li>
+            <li>999999999</li>
+          </ul>
+        </div>
+
+        <div className='school-social info-margin-top'>
+          <ul className='ul-properties hz-list'>
+            <li>
+              <Link to="#"><FaInstagram /></Link>
+            </li>
+            <li>
+              <Link to="#"><FaYoutube /></Link>
+            </li>
+            <li>
+              <Link to="#"><FaFacebook /></Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className='school-email info-margin-top'>
+          <ul className='ul-properties'>
+            <li>abc@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+
 
 
     </div>
